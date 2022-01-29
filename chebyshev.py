0b04f0add4b7cab0ab0585756834a52958d80676
face0u0 <me@face0u0.net>
2022-01-29 15:14:21 +0900

    push

---
+++ chebyshev.py
@@ -0,0 +1,17 @@
+from ica import *
+import matplotlib.pyplot as plt
+import numpy as np
+
+initials = np.linspace(-0.01, 0.01, 100)
+xx, yy = np.meshgrid(initials, initials)
+cps = np.array([const_powerd_samples(2, i, 1000).real for i in initials])
+c = correlation(cps)
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+surf = ax.plot_surface(xx, yy, c, cmap='bwr', linewidth=0)
+fig.colorbar(surf)
+ax.set_title("Surface Plot")
+fig.show()
+plt.show()
+pass
