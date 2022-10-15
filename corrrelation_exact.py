17f4a0c16e2604871d7bd48d671afecc3fedf90f
face0u0 <me@face0u0.net>
2022-10-15 12:55:21 +0900

    pyusH

--- corrrelation_exact.py
+++ corrrelation_exact.py
@@ -8,10 +8,12 @@ q = 2
 print((p-1)/2)
 print(is_prime(int((p-1)/2)))
 X1 = primitive_root_code(p, q, 1)
-X2 = primitive_root_code(p, q, 1)
+X2 = primitive_root_code(p, q, 2)
 
+X1_1 = np.append(1, X1)
+X2_1 = np.append(1, X2)
 cor = []
-print(np.vdot(np.append(1, X1), np.append(1, X2)))
+print(np.vdot(X1_1, X2_1))
 print(np.vdot(X1, X2))
 
 for i in range(p):
