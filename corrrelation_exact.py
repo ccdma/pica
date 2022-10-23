47a440b8c2d07f89547b8c1c254fd07e344577eb
face0u0 <me@face0u0.net>
2022-10-23 22:44:21 +0900

    Push

--- corrrelation_exact.py
+++ corrrelation_exact.py
@@ -1,4 +1,3 @@
-from pandas import qcut
 from lib.ica import *
 import matplotlib.pyplot as plt
 
@@ -7,19 +6,18 @@ q = 2
 
 print((p-1)/2)
 print(is_prime(int((p-1)/2)))
-X1 = primitive_root_code(p, q, 1)
-X2 = primitive_root_code(p, q, 2)
 
-X1_1 = np.append(1, X1)
-X2_1 = np.append(1, X2)
 cor = []
-print(np.vdot(X1_1, X2_1))
-print(np.vdot(X1, X2))
 
-for i in range(p):
-	cor.append(np.vdot(X1, np.roll(X2, i)))
+for i in range(p-2):
+	X1 = primitive_root_code(p, q, 1)
+	X2 = primitive_root_code(p, q, i+2)
 
-plt.plot(np.array(cor).real/p)
+	X1_1 = np.append(1, X1)
+	X2_1 = np.append(1, X2)
+	cor.append(np.vdot(X1_1, X2_1))
+
+plt.scatter(np.array(cor).real/p, np.array(cor).imag/p)
 plt.show()
 
 
