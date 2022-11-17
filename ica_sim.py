775f5ee6510554eadd148ef57d941637968317f9
face0u0 <app@mu2chn.net>
2022-11-17 23:47:30 +0900

    push

--- ica_sim.py
+++ ica_sim.py
@@ -4,23 +4,23 @@ ICAを用いてパワー一定カオス拡散符号の復元を行う
 import matplotlib.pyplot as plt
 import numpy as np
 import lb
-import dataclasses, sys
+import dataclasses, sys, warnings
 import dataclass_csv
 
 np.random.seed(1)
 
 @dataclasses.dataclass
 class EachReport:
-    ber: int
-    snr: float
+	ber: int
+	snr: float
 
 @dataclasses.dataclass
 class SummaryReport:
-    K: int
-    N: int
-    ber: float
-    snr: float
-    complete: int
+	K: int
+	N: int
+	ber: float
+	snr: float
+	complete: int
 
 """
 K: number of Users
@@ -52,21 +52,26 @@ def ica(K: int, N: int, snr: float):
 	return EachReport(ber=ber, snr=lb.snr(MIXED, AWGN))
 
 N = 1019
-expected_snr = 30
+expected_snr = 10
 dataclass_csv.DataclassWriter(sys.stdout, [], SummaryReport).write()
 for K in range(2, N):
-    ber_sum = 0
-    snr_sum = 0
-    complete = 0
-    for trial in range(100):
-        report = ica(K, N, expected_snr)
-        ber_sum += report.ber
-        snr_sum += report.snr
-        complete += 1
-    dataclass_csv.DataclassWriter(sys.stdout, [SummaryReport(
-        K=K,
-        N=N,
-        ber=ber_sum/complete,
-        snr=snr_sum/complete,
-        complete=complete
-    )], SummaryReport).write(skip_header=True)
+	ber_sum = 0
+	snr_sum = 0
+	complete = 0
+	for trial in range(1000):
+		with warnings.catch_warnings():
+			warnings.filterwarnings('error')
+			try:
+				report = ica(K, N, expected_snr)
+				ber_sum += report.ber
+				snr_sum += report.snr
+				complete += 1
+			except Warning as e:
+				pass
+	dataclass_csv.DataclassWriter(sys.stdout, [SummaryReport(
+		K=K,
+		N=N,
+		ber=ber_sum/complete,
+		snr=snr_sum/complete,
+		complete=complete
+	)], SummaryReport).write(skip_header=True)
