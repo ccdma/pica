ba87d98e283f48cbccce6fdc864f91f247151152
face0u0 <app@mu2chn.net>
2022-11-17 21:38:38 +0900

    push

--- _snr_sttdev.py
+++ _snr_sttdev.py
@@ -1,5 +1,6 @@
 from numpy import sqrt
-from lb.ica import *
+import lb
+import numpy as np
 import matplotlib.pyplot as plt
 
 SAMPLES = 10000
@@ -12,7 +13,7 @@ for stddev in stddevs:
 	norms = norms1 + 1j * norms2
 	rpn = np.power(np.abs(norms1), 2).sum()
 	cpn = np.power(np.abs(norms), 2).sum()
-	rps = np.power(np.abs(chebyt_code(2, 0.1, SAMPLES)), 2).sum()
+	rps = np.power(np.abs(lb.chebyt_code(2, 0.1, SAMPLES)), 2).sum()
 	ravgs.append(10*np.log10(rps/rpn))
 	cavgs.append(10*np.log10(SAMPLES/cpn))
 plt.plot(stddevs, ravgs, label="real")
