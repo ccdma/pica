78f42e75b67cf529520cffecbbefdd1557760ec3
mu2chn <app@mu2chn.net>
2022-11-22 12:47:25 +0900

    push

--- code_spec.py
+++ code_spec.py
@@ -4,7 +4,6 @@
 import lb
 import numpy as np
 import matplotlib.pyplot as plt
-
 """
 2つの系列の相互相関を求める
 """
@@ -14,19 +13,14 @@ def get_correlations(code_1: np.ndarray, code_2: np.ndarray):
 	code_2_expand = lb.each_row_roll(np.tile(code_2[None], (code_len, 1)), np.arange(code_len))
 	return np.mean(code_1_expand * np.conj(code_2_expand), axis=1) # np.vdot
 
-pq_list = [(61, 2)]
-code_1 = lb.mixed_primitive_root_code_without1(pq_list, 1)
+pq_list = [(101, 2), (3, 2)]
+code_1 = lb.mixed_primitive_root_code(pq_list, 1)
 code_len = code_1.shape[0]
 
-correlations = []
-for_range = range(2, code_len)
-for k in for_range:
-	code_2 = lb.mixed_primitive_root_code_without1(pq_list, k)
-	correlations.append(np.argmax(np.abs(get_correlations(code_1, code_2))))
+code_2 = np.roll(lb.mixed_primitive_root_code(pq_list, 2), 0)
 
 # 相関をプロット
-plt.plot(for_range, correlations)
-# plt.plot(np.abs(get_correlations(code_1, lb.mixed_primitive_root_code_without1(pq_list, 10))))
+plt.plot(np.abs(get_correlations(code_1, code_2)))
 plt.title(f"correlation of X(k1=1) and X(k2)")
 plt.xlabel("roll")
 plt.ylabel("correlation")
@@ -34,8 +28,10 @@ plt.tight_layout()
 plt.show()
 
 # # IQをプロット
-# plt.scatter(code_1.real, code_1.imag, s=1)
-# plt.plot(code_1.real, code_1.imag, lw=0.2)
-# plt.title(f"IQ plot of (p, q)={pq_list}")
+# plt.scatter(code_1.real, code_1.imag, s=2)
+# plt.plot(code_1.real, code_1.imag, lw=2.5)
+# plt.scatter(code_2.real, code_2.imag, s=2)
+# plt.plot(code_2.real, code_2.imag, lw=1.0)
+# plt.title(f"IQ plot of (p, q)={pq_list}: X(k1=1) and X(k2=2,roll=1)")
 # plt.gca().set_aspect('equal','datalim')
 # plt.show()
--- sim_cdma.py
+++ sim_cdma.py
@@ -64,10 +64,10 @@ def cdma(K: int, N: int, snr: float, sync: bool, seed: int) -> EachReport:
 	bpsk_data = np.complex64(bits)
 	
 	B = np.repeat(bpsk_data, N, axis=0).T
-	S = np.array([lb.mixed_primitive_root_code([(5, 2), (13, 2)], k) for k in rand.sample(range(1, N+1), K)])
+	# S = np.array([lb.mixed_primitive_root_code([(3, 2), (11, 2)], k) for k in rand.sample(range(1, N+1), K)])
 	# S = np.array([lb.primitive_root_code(N, 2, k) for k in rand.sample(range(1, N+1), K)])
 	# S = np.array([lb.primitive_root_code(N+1, 2, k)[1:] for k in rand.sample(range(1, N+1), K)])
-	# S = np.array([lb.const_power_code(2, np.random.rand(), N) for _ in range(1, K+1)])
+	S = np.array([lb.const_power_code(2, np.random.rand(), N) for _ in range(1, K+1)])
 
 	if not sync: S = lb.each_row_roll(S, np.random.randint(0, N, K))
 	T = B * S
@@ -101,7 +101,7 @@ def do_trial(K: int, N: int):
 def main():
 	DataclassWriter(sys.stdout, [], SummaryReport, delimiter=DELIMITER).write()
 
-	N = 65
+	N = 33
 	with futu.ProcessPoolExecutor(max_workers=MAX_WORKERS) as executor:
 		futures = [executor.submit(do_trial, K, N) for K in range(2, N)]
 		for future in futu.as_completed(futures):
