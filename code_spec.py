8003e2c57c6121f568bcbef36cf8315283e97930
face0u0 <app@mu2chn.net>
2022-12-01 23:08:58 +0900

    push

--- code_spec.py
+++ code_spec.py
@@ -5,34 +5,6 @@ import lb
 import numpy as np
 import matplotlib.pyplot as plt
 
-# def find_group(G):
-# 	def dfs(v, G, seen, group):
-# 		group.append(v)
-# 		# 頂点 v を探索済みにする
-# 		seen[v] = True
-# 		# G[v] に含まれる頂点 v2 について、
-# 		for v2, val in enumerate(G[v]):
-# 			# 直交していないのでスキップ
-# 			if val > 1e-10: continue
-# 			# v2 がすでに探索済みならば、スキップする
-# 			if seen[v2]: continue
-# 			# v2 始点で深さ優先探索を行う (関数を再帰させる)
-# 			dfs(v2, G, seen, group)
-# 		return
-# 	N = G.shape[0]
-# 	seen = [False for _ in range(N)]    # seen[v]：頂点 v が探索済みなら true, そうでないなら false
-# 	groups = []
-# 	# 全ての頂点について
-# 	for v in range(N):
-# 		# 頂点 v がすでに訪問済みであれば、スキップ
-# 		if seen[v]: continue
-# 		# そうでなければ、頂点 v を含む連結成分は未探索
-# 		# 深さ優先探索で新たに訪問し、答えを 1 増やす
-# 		group = []
-# 		dfs(v, G, seen, group)
-# 		groups.append(group)
-# 	return groups
-
 """
 (p,q)が原始根となるpを取得する
 """
@@ -44,28 +16,38 @@ def find_p_xxx(q: int=2):
 	return p_list
 
 q = 2
-p_b = 3
-pq_list = [(p_b, q),(5, q),(11, q),]
+pq_list = [(3, q),(5, q),]
 
-code_1 = lb.mixed_primitive_root_code(pq_list, 1)
-code_len = code_1.shape[0]
+code_len = lb.mixed_primitive_root_code(pq_list, 1).shape[0]
+
 c_max = []
-c_avg = []
-for k in range(1, code_len):
-	code_2 = lb.mixed_primitive_root_code(pq_list, k)
-	cc = lb.cross_correlations(code_1, code_2)
-	c_max.append(np.max(np.abs(cc)))
-	c_avg.append(np.mean(np.abs(cc)))
+for k1 in range(1, code_len):
+
+	each_c_max = []
+	c_max.append(each_c_max)
+	code_1 = lb.mixed_primitive_root_code(pq_list, k1)
+	code_len = code_1.shape[0]
+
+	k2_range = range(1, code_len)
+	
+	for k2 in k2_range:
+		code_2 = lb.mixed_primitive_root_code(pq_list, k2)
+		cc = lb.cross_correlations(code_1, code_2)
+		each_c_max.append(np.max(np.abs(cc)))
+
+c_max = np.array(c_max)
+
+# print(c_max)
+np.savetxt("a.csv", c_max, delimiter=",")
 
 # 相関をプロット
-# plt.plot(c_max)
-# plt.plot(c_avg)
-plt.plot(np.abs(lb.cross_correlations(code_1, lb.mixed_primitive_root_code(pq_list, 2))))
-plt.title(f"correlation of X(k1=1) and X(k2=2)")
-plt.xlabel("roll")
-plt.ylabel("correlation")
-plt.tight_layout()
-plt.show()
+# plt.scatter(k2_range, c_max, s=2)
+# plt.plot(k2_range, c_max, lw=1)
+# plt.title(f"correlation of X(k1={k1}) and X(k2)")
+# plt.xlabel("k2")
+# plt.ylabel("correlation")
+# plt.tight_layout()
+# plt.show()
 
 # # IQをプロット
 # plt.scatter(code_1.real, code_1.imag, s=1)
