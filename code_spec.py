468077821e68a497825e9a61da29a5b3a98fd534
mu2chn <app@mu2chn.net>
2022-12-08 17:05:26 +0900

    pusH

--- code_spec.py
+++ code_spec.py
@@ -28,37 +28,37 @@ code_len = lb.mixed_primitive_root_code(pq_list, 1).shape[0]
 k_range = range(1, code_len+1)
 
 # 相互相関が最大となる値を集めたk^2テーブル
-c_max = []
-for k1 in k_range:
-	each_c_max = []
-	c_max.append(each_c_max)
-	code_1 = lb.mixed_primitive_root_code(pq_list, k1)
-	code_len = code_1.shape[0]
-	k2_range = range(1, code_len)
+# c_max = []
+# for k1 in k_range:
+# 	each_c_max = []
+# 	c_max.append(each_c_max)
+# 	code_1 = lb.mixed_primitive_root_code(pq_list, k1)
+# 	code_len = code_1.shape[0]
+# 	k2_range = range(1, code_len)
 	
-	for k2 in k_range:
-		code_2 = lb.mixed_primitive_root_code(pq_list, k2)
-		cc = lb.cross_correlations(code_1, code_2)
-		each_c_max.append(np.max(np.abs(cc)))
-c_max = np.array(c_max)
+# 	for k2 in k_range:
+# 		code_2 = lb.mixed_primitive_root_code(pq_list, k2)
+# 		cc = lb.cross_correlations(code_1, code_2)
+# 		each_c_max.append(np.max(np.abs(cc)))
+# c_max = np.array(c_max)
 
 # 平均的な自己相関
-# self_corr_avg = []
+# self_corr_max = []
 # for k1 in k_range:
 # 	code = lb.mixed_primitive_root_code(pq_list, k1)
-# 	self_corr_avg.append(np.mean(np.abs(self_correlations(code))))
+# 	self_corr_max.append(np.max(np.abs(self_correlations(code))))
 
 # csv書き出し
-np.savetxt("a.csv", c_max, delimiter=",")
+# np.savetxt("a.csv", c_max, delimiter=",")
 
 # 相関をプロット
-# plt.scatter(k_range, self_corr_avg, s=2)
-# plt.plot(k_range, self_corr_avg, lw=1)
-# plt.title(f"mean of self correlation of X(k1)")
-# plt.xlabel("k1")
-# plt.ylabel("correlation")
-# plt.tight_layout()
-# plt.show()
+# plt.scatter(self_correlations(lb.mixed_primitive_root_code(pq_list, 1)), s=2)
+plt.plot(np.abs(self_correlations(lb.mixed_primitive_root_code(pq_list, 2))), lw=1)
+plt.title(f"mean of self correlation of X(k1)")
+plt.xlabel("k1")
+plt.ylabel("correlation")
+plt.tight_layout()
+plt.show()
 
 # # IQをプロット
 # plt.scatter(code_1.real, code_1.imag, s=1)
