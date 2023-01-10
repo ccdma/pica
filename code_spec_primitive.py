b68a47b1e17d7468011caeb62342f280b192703d
mu2chn <app@mu2chn.net>
2023-01-10 16:27:49 +0900

    puh

--- code_spec_primitive.py
+++ code_spec_primitive.py
@@ -27,41 +27,48 @@ pq_list = [(3,2),(5,3),(7,5)]
 code_len = lb.mixed_primitive_root_code(pq_list, 1).shape[0]
 k_range = range(1, code_len+1)
 
-# 相互相関が最大となる値を集めたk^2テーブル
-c_max = []
-for k1 in k_range:
-	each_c_max = []
-	c_max.append(each_c_max)
-	code_1 = lb.mixed_primitive_root_code(pq_list, k1)
-	code_len = code_1.shape[0]
-	k2_range = range(1, code_len)
+# # 相互相関が最大となる値を集めたk^2テーブル
+# c_max = []
+# for k1 in k_range:
+# 	each_c_max = []
+# 	c_max.append(each_c_max)
+# 	code_1 = lb.mixed_primitive_root_code(pq_list, k1)
+# 	code_len = code_1.shape[0]
+# 	k2_range = range(1, code_len)
 	
-	for k2 in k_range:
-		code_2 = lb.mixed_primitive_root_code(pq_list, k2)
-		cc = lb.cross_correlations(code_1, code_2)
-		each_c_max.append(np.max(np.abs(cc)))
-c_max = np.array(c_max)
+# 	for k2 in k_range:
+# 		code_2 = lb.mixed_primitive_root_code(pq_list, k2)
+# 		cc = lb.cross_correlations(code_1, code_2)
+# 		each_c_max.append(np.max(np.abs(cc)))
+# c_max = np.array(c_max)
 
-# 平均的な自己相関
-self_corr_max = []
-for k1 in k_range:
-	code = lb.mixed_primitive_root_code(pq_list, k1)
-	self_corr_max.append(np.mean(np.abs(self_correlations(code))))
+# # 平均的な自己相関
+# self_corr_max = []
+# for k1 in k_range:
+# 	code = lb.mixed_primitive_root_code(pq_list, k1)
+# 	self_corr_max.append(np.mean(np.abs(self_correlations(code))))
 
 # csv書き出し
-np.savetxt("a.csv", self_corr_max, delimiter=",")
+# np.savetxt("a.csv", self_corr_max, delimiter=",")
+
+# code_1 = lb.primitive_root_code(173, 2, 1)[1:]
+# code_2 = lb.primitive_root_code(173, 2, 2)[1:]
+
+code_1 = lb.const_power_code(2, np.sqrt(2), 100)
+# code_2 = lb.const_power_code(2, np.sqrt(3), 173)
 
 # 相関をプロット
-# plt.plot(k_range, c_max[0], lw=1)
-# # plt.title(f"mean of self correlation of X(k1)")
-# plt.xlabel("k1")
-# plt.ylabel("correlation")
-# plt.tight_layout()
-# plt.show()
+plt.plot(np.abs(self_correlations(code_1)), lw=1)
+# plt.title(f"cross correlation of X(from=sqrt(2))")
+# plt.ylim(0, 1)
+plt.xlabel("roll")
+plt.ylabel("correlation")
+plt.tight_layout()
+plt.show()
 
 # # IQをプロット
-# plt.scatter(code_1.real, code_1.imag, s=1)
-# plt.plot(code_1.real, code_1.imag, lw=1.0)
+# plt.scatter(code_1.real, code_1.imag, s=0.8)
+# plt.plot(code_1.real, code_1.imag, lw=0.2)
 # plt.title(f"IQ plot of (p, q)={pq_list}: X(k1=1) and X(k2=2,roll=1)")
 # plt.gca().set_aspect('equal','datalim')
 # plt.show()
