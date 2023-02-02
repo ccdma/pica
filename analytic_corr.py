dbffca8f49ef8a26f34299de9eda5a2cc3fa5cb8
mu2chn <app@mu2chn.net>
2023-02-02 17:58:14 +0900

    pus

---
+++ analytic_corr.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+k1 = 1
+k2 = 2
+
+p1, q1 = 5, 2
+p2, q2 = 3, 2
+
+_sum = 0
+theta1 = 1
+theta2 = 1
+for i in range(p1*p2):
+    _sum += np.exp(-2j*np.pi*())
+    theta1 = theta1
