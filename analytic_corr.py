a7b99f06669de134a6df7dbad59f8e8c26c1f4c9
mu2chn <app@mu2chn.net>
2023-02-02 18:00:17 +0900

    pus

--- analytic_corr.py
+++ analytic_corr.py
@@ -7,8 +7,7 @@ p1, q1 = 5, 2
 p2, q2 = 3, 2
 
 _sum = 0
-theta1 = 1
-theta2 = 1
 for i in range(p1*p2):
-    _sum += np.exp(-2j*np.pi*())
-    theta1 = theta1
+    theta1 = pow(q1, i, p1)/p1
+    theta2 = pow(q2, i, p2)/p2
+    _sum += np.exp(-2j*np.pi*(theta1+theta2)*(k1+k2))
