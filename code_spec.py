bff02fe06f32aaea3fc0ed27eb4fc548851b5f8f
face0u0 <app@mu2chn.net>
2023-03-07 00:16:26 +0900

    initial commit

--- code_spec.py
+++ code_spec.py
@@ -12,14 +12,11 @@ def self_correlations(code):
 	return lb.cross_correlations(code, code)[1:]
 
 q = 2
-pq_set = [(11, 2), (3, 2)]
+pq_set = [(67, 2)]
 
 code_len = lb.mixed_primitive_root_code(pq_set, 1).shape[0]
 
-k_range = range(1, code_len+1)
-
 code_1 = lb.mixed_primitive_root_code(pq_set, 1)
-code_2 = lb.mixed_primitive_root_code(pq_set, 2)
 
 # 相関をプロット
 # plt.plot(np.abs(lb.cross_correlations(code_1, code_1)), lw=1)
@@ -30,7 +27,7 @@ code_2 = lb.mixed_primitive_root_code(pq_set, 2)
 
 # # IQをプロット
 plt.scatter(code_1.real, code_1.imag, s=0.8)
-plt.plot(code_1.real, code_1.imag, lw=0.2)
+plt.plot(code_1.real, code_1.imag, lw=0.3)
 # plt.title(f"IQ plot of (p, q)={pq_set}: X(k1=1) and X(k2=2,roll=1)")
 plt.gca().set_aspect('equal','datalim')
 plt.show()
--- code_spec_multi.py
+++ code_spec_multi.py
@@ -5,18 +5,18 @@ import lb, itertools, random
 import numpy as np
 import matplotlib.pyplot as plt
 
-pq_comb = list(itertools.combinations(lb.find_pq(range(20), [2]), 2)) + list(itertools.combinations(lb.find_pq(range(20), [3]), 2))
+pq_comb = list(itertools.combinations(lb.find_pq(range(2, 50), range(2, 30)), 2))
 pq_comb = list(filter(lambda pq_set: pq_set[0][0] != pq_set[1][0], pq_comb))
-# random.shuffle(pq_comb)
+random.shuffle(pq_comb)
 
-fig, axes = plt.subplots(ncols=5, nrows=2)	# plt.Figure, plt.Axes[]
+fig, axes = plt.subplots(ncols=1, nrows=1, squeeze=False)	# plt.Figure, plt.Axes[]
 
 for ax, pq_set in zip(
 		itertools.chain.from_iterable(axes),
 		pq_comb
 	):
 	code_1 = lb.mixed_primitive_root_code(pq_set, 1)
-	ax.scatter(code_1.real, code_1.imag, s=0.8)
+	ax.scatter(code_1.real, code_1.imag, s=0.4)
 	ax.plot(code_1.real, code_1.imag, lw=0.2)
 	ax.set_title(f"{pq_set}")
 	ax.set_aspect('equal')
