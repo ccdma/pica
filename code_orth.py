4f403a4b88679d5367440b34272f06cc71ca668c
face0u0 <app@mu2chn.net>
2023-03-18 18:12:10 +0900

    ps

--- code_orth.py
+++ code_orth.py
@@ -3,8 +3,23 @@ import numpy as np
 import matplotlib.pyplot as plt
 np.set_printoptions(linewidth=1000)
 
-phi_m = lb.args_index(lb.primitive_root_code(pq[0], pq[1], 1), pq[0])
+pq1 = (11, 2)
+phi_1 = lb.args_index(lb.primitive_root_code(pq1[0], pq1[1], 1), pq1[0])
+
+pq2 = (3, 2)
+phi_2 = lb.args_index(lb.primitive_root_code(pq2[0], pq2[1], 1), pq2[0])
+
 k1 = 1
 k2 = 2
-t0 = 1
-print(np.mod(phi_m*k1 - np.roll(phi_m, -t0)*k2, pq[0]))
+t0 = 4
+
+print(phi_1)
+print(np.roll(phi_1, -t0))
+
+i1 = np.mod(phi_1*k1 - np.roll(phi_1, -t0)*k2, pq1[0])
+i2 = np.mod(phi_2*k1 - np.roll(phi_2, -t0)*k2, pq2[0])
+s = 0
+for v1 in i1:
+	for v2 in i2:
+		s += v1/pq1[0] + v2/pq2[0]
+print(s)
