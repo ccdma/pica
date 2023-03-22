637a9f1ae37b638da71c0b92fbd5ecbe645ca783
mu2chn <app@mu2chn.net>
2023-03-22 16:36:22 +0900

    us

--- code_orth.py
+++ code_orth.py
@@ -9,15 +9,16 @@ phi_1 = lb.args_index(lb.primitive_root_code(pq1[0], pq1[1], 1), pq1[0])
 pq2 = (3, 2)
 phi_2 = lb.args_index(lb.primitive_root_code(pq2[0], pq2[1], 1), pq2[0])
 
-k1 = 1
-k2 = 4
+k1 = 2
+k2 = 1
 
 # for t0 in range(pq1[0]*pq2[0]):
-t0 = 4
+t0 = 0
+
 i1 = np.mod(phi_1*k1 - np.roll(phi_1, -t0)*k2, pq1[0])
 i2 = np.mod(phi_2*k1 - np.roll(phi_2, -t0)*k2, pq2[0])
 
 # print(i1)
-# print(i2)
+print(i2)
 
 # print(np.sum(np.exp(-2j*np.pi*i1/pq1[0]))*np.sum(np.exp(-2j*np.pi*i2/pq2[0])))
--- code_spec.py
+++ code_spec.py
@@ -11,14 +11,14 @@ np.set_printoptions(linewidth=1000)
 def self_correlations(code):
 	return lb.cross_correlations(code, code)[1:]
 
-pq_set = [(3, 2), (17, 5)]
+pq_set = [(5, 2), (17, 5)]
 
 code_len = lb.mixed_primitive_root_code(pq_set, 1).shape[0]
 
 k_range = range(1, code_len+1)
 
 code_1 = lb.mixed_primitive_root_code(pq_set, 1)
-code_2 = lb.mixed_primitive_root_code(pq_set, 4)
+code_2 = lb.mixed_primitive_root_code(pq_set, 6)
 
 corr_max = np.array([
 	[
@@ -28,11 +28,11 @@ corr_max = np.array([
 np.savetxt("a.csv", corr_max, delimiter="\t") # fmt='%.5f'
 
 # 相関をプロット
-# plt.plot(np.abs(lb.cross_correlations(code_1, code_2)), lw=1)
-# plt.title(f"cross correlation {pq_set}")
-# plt.xlabel("roll")
-# plt.ylabel("correlation")
-# plt.show()
+plt.plot(np.abs(lb.cross_correlations(code_1, code_2)), lw=1)
+plt.title(f"cross correlation {pq_set}")
+plt.xlabel("roll")
+plt.ylabel("correlation")
+plt.show()
 
 # # IQをプロット
 # plt.scatter(code_1.real, code_1.imag, s=1.0)
