8855ff5d2a52d5f8c3f945ca63819cbc238510fa
mu2chn <app@mu2chn.net>
2023-03-31 12:19:40 +0900

    push

--- code_orth.py
+++ code_orth.py
@@ -3,40 +3,20 @@ import numpy as np
 import matplotlib.pyplot as plt
 np.set_printoptions(linewidth=1000)
 
-pq1 = (11, 2)
-phi_1 = lb.args_idx(lb.primitive_root_code(pq1[0], pq1[1], 1), pq1[0])
+def phi(pq: lb.pq, t: int) -> int:
+    if t % pq[0] == 0: return 0
+    return pow(pq[1], (t % pq[0] - 1), pq[0])
 
-pq2 = (3, 2)
-phi_2 = lb.args_idx(lb.primitive_root_code(pq2[0], pq2[1], 1), pq2[0])
+
+pq = (11, 2)
+phis = lb.args_idx(lb.primitive_root_code(pq[0], pq[1], 1), pq[0])
 
 k1 = 2
 k2 = 1
-
-# for t0 in range(pq1[0]*pq2[0]):
 t0 = 0
 
-i1 = np.mod(phi_1*k1 - np.roll(phi_1, -t0)*k2, pq1[0])
-i2 = np.mod(phi_2*k1 - np.roll(phi_2, -t0)*k2, pq2[0])
-
-# print(i1)
-print(i2)
-
-# print(np.sum(np.exp(-2j*np.pi*i1/pq1[0]))*np.sum(np.exp(-2j*np.pi*i2/pq2[0])))
-
-for pq1 in lb.find_pq(range(2, 60), range(2, 60)):
-    p = pq1[0]
-    phi_1 = lb.args_idx(lb.primitive_root_code(pq1[0], pq1[1], 1), pq1[0])
-    phi = list(map(int, phi_1.tolist()))
-    for t1 in range(p):
-        for t2 in range(p):
-            exp = phi[(t1+t2)%p]
-            # print(t1, t2, exp, end=" ")
-            if t1 == 0:
-                assert exp == (phi[t2])%p
-            else:
-                if t1 + t2 < p:
-                    assert exp == (phi[t1]*phi[(t2+1)%p])%p
-                if t1 + t2 == p:
-                    assert exp == 0
-                if t1 + t2 > p:
-                    assert exp == (phi[t1]*phi[t2])%p
+for t0 in range(pq[0]):
+    print(f"### {t0}")
+    print(f"phi(t0)\t= {phi(pq, t0)}")
+    print(f"phi(t0+1)\t= {phi(pq, t0+1)}")
+# print(f"-k2 phi(t0)\t= {-k2*phi(pq, t0)}")
