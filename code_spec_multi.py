d90e275a27c7de2e55032f951cf63171331d5d22
face0u0 <app@mu2chn.net>
2023-04-08 14:18:25 +0900

    update

--- code_spec_multi.py
+++ code_spec_multi.py
@@ -14,23 +14,16 @@ class CodeGen:
 		return lb.mixed_primitive_root_code(self.pq_set, self.k)
 	
 	def __str__(self) -> str:
-		return f"(pq:{self.pq_set},k:{self.k})"
+		return f"(p,q)={self.pq_set[0]},{self.pq_set[0]}"
 
-gens: list[CodeGen] = []
-for pq in lb.find_pq([11], range(2, 7)) :
-	for k in range(1, 4):
-		gens.append(CodeGen([(3, 2), pq,], k))
-
-fig, axes = plt.subplots(ncols=len(gens), nrows=len(gens), squeeze=False)	# plt.Figure, plt.Axes[]
+gens: list[CodeGen] = [CodeGen([(3, 2), (5, 2)], 1), CodeGen([(5, 2), (7, 3)], 1)]
+fig, axes = plt.subplots(ncols=len(gens), nrows=1, squeeze=False)	# plt.Figure, plt.Axes[]
 
 for i in range(len(gens)):
-	for j in range(len(gens)):
-		code_1 = gens[i].code()
-		code_2 = gens[j].code()
-		ax = axes[i][j]
-		ax.plot(np.abs(lb.cross_correlations(code_1, code_2)))
-		ax.set_ylim(0, 1)
-		ax.set_title(f"{gens[i]}x{gens[j]}", fontsize=8)
+	code_1 = gens[i].code()
+	ax = axes[0][i]
+	lb.plt.iq(ax, code_1, s=3, lw=1)
+	ax.set_title(f"{gens[i]}", fontsize=10)
 
-fig.tight_layout(h_pad=-0.9, w_pad=-0.5)
+fig.tight_layout()
 plt.show()
