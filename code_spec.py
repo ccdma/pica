d1578414901c9866c97f62d5f84a5a03f876391f
mu2chn <app@mu2chn.net>
2023-04-17 18:02:10 +0900

    push

--- code_spec.py
+++ code_spec.py
@@ -11,32 +11,32 @@ np.set_printoptions(linewidth=1000)
 def self_correlations(code):
 	return lb.cross_correlations(code, code)[1:]
 
-pq_set = [(3, 2), (5, 2)]
+pq_set = [(17, 3)]
 
 code_len = lb.mixed_primitive_root_code(pq_set, 1).shape[0]
 
 k_range = range(1, code_len+1)
 
-code_1 = lb.mixed_primitive_root_code(pq_set, 5)
-code_2 = lb.mixed_primitive_root_code(pq_set, 1)
+code_1 = lb.mixed_primitive_root_code(pq_set, 1)
+# code_2 = lb.mixed_primitive_root_code(pq_set, 1)
 
-corr_max = np.array([
-	[
-		np.max(np.abs(lb.cross_correlations(lb.mixed_primitive_root_code(pq_set, k_1), lb.mixed_primitive_root_code(pq_set, k_2)))) for k_1 in k_range
-	] for k_2 in k_range
-])
-np.savetxt("a.csv", corr_max, delimiter="\t") # fmt='%.5f'
+# corr_max = np.array([
+# 	[
+# 		np.max(np.abs(lb.cross_correlations(lb.mixed_primitive_root_code(pq_set, k_1), lb.mixed_primitive_root_code(pq_set, k_2)))) for k_1 in k_range
+# 	] for k_2 in k_range
+# ])
+# np.savetxt("a.csv", corr_max, delimiter="\t") # fmt='%.5f'
 
 fig, ax = plt.subplots()
 
 # 相関をプロット
-ax.plot(np.abs(lb.cross_correlations(code_1, code_2)), lw=1)
-ax.set_title(f"cross correlation {pq_set}")
-ax.set_xlabel("roll")
-ax.set_ylabel("correlation")
-plt.show()
+# ax.plot(np.abs(lb.cross_correlations(code_1, code_2)), lw=1)
+# ax.set_title(f"cross correlation {pq_set}")
+# ax.set_xlabel("roll")
+# ax.set_ylabel("correlation")
+# plt.show()
 
 # # IQをプロット
-# lb.plt.iq(ax, code_1)
-# ax.set_title(f"IQ plot of (p, q)={pq_set}: X(k1=1) and X(k2=2,roll=1)")
-# plt.show()
+lb.plt.iq(ax, code_1, s=8, lw=1)
+ax.set_title(f"IQ plot of (p, q)={pq_set}")
+plt.show()
