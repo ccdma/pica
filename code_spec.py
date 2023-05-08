364256aff88c1f5ee2c0bfa111f47616b05a1d9d
mu2chn <app@mu2chn.net>
2023-05-08 16:12:33 +0900

    push

--- code_spec.py
+++ code_spec.py
@@ -4,21 +4,24 @@
 import lb, numba, math
 import numpy as np
 import matplotlib.pyplot as plt
+
 np.set_printoptions(linewidth=1000)
+plt.rcParams['font.size'] = 15
+
 """
 自己相関を計算（冒頭の1を含まない）
 """
 def self_correlations(code):
 	return lb.cross_correlations(code, code)[1:]
 
-pq_set = [(3, 2), (5, 2), (7, 3)]
+pq_set = [(3, 2), (5, 2)]
 
 code_len = lb.mixed_primitive_root_code(pq_set, 1).shape[0]
 
 k_range = range(1, code_len+1)
 
 code_1 = lb.mixed_primitive_root_code(pq_set, 1)
-# code_2 = lb.mixed_primitive_root_code(pq_set, 1)
+code_2 = lb.mixed_primitive_root_code(pq_set, 4)
 
 # np.savetxt("a.csv", np.array([
 # 	[
@@ -29,13 +32,14 @@ code_1 = lb.mixed_primitive_root_code(pq_set, 1)
 fig, ax = plt.subplots()
 
 # 相関をプロット
-# ax.plot(np.abs(lb.cross_correlations(code_1, code_2)), lw=1)
-# ax.set_title(f"cross correlation {pq_set}")
-# ax.set_xlabel("roll")
-# ax.set_ylabel("correlation")
-# plt.show()
+ax.plot(np.abs(lb.cross_correlations(code_1, code_2)), marker='o', lw=1)
+ax.set_title(f"cross correlation (p, q)={pq_set}: k=1,4")
+ax.set_xlabel("lag (t0)")
+ax.set_ylabel("correlation")
+fig.tight_layout()
+plt.show()
 
 # # IQをプロット
-lb.plt.iq(ax, code_1, s=8, lw=1)
-ax.set_title(f"IQ plot of (p, q)={pq_set}")
-plt.show()
+# lb.plt.iq(ax, code_1, s=8, lw=1)
+# ax.set_title(f"IQ plot of (p, q)={pq_set}")
+# plt.show()
