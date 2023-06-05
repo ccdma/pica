98cb52f4fda1fd75cb7d270799c1b2b734fe1226
mu2chn <app@mu2chn.net>
2023-06-05 12:46:34 +0900

    update

--- an_round_once.py
+++ an_round_once.py
@@ -1,19 +1,19 @@
 import numpy as np
 import math
 
-P = 17
+P = 19
 DATA = np.int64(range(P))
 EXP = np.exp(2j * np.pi * np.arange(P) / P)
 
 def comb(r, start, combset):
-  if r == 0:
-    _sum = np.abs(np.sum(EXP[combset]))
-    if _sum < 10e-10:
-        print(combset, _sum)
-    return
+    if r == 0:
+        _sum = np.abs(np.sum(EXP[combset]))
+        if _sum < 10e-10:
+            print(combset, _sum)
+        return
 
-  for i in range(start, P):
-    comb(r - 1, i, combset + [DATA[i]])
+    for i in range(start, P):
+        comb(r - 1, i, combset + [DATA[i]])
 
-print(math.comb(P*2-1, P-1))
+print(math.comb(P*2-1, P))
 comb(P, 0, [])
